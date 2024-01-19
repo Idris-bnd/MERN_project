6f8faa97f08236c61a7869ebcf09e3c4c23ecde7
Idris BEN AHMED <idris.pro83@yahoo.com>
2024-01-19 18:50:21 +0100

    update and delete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,10 +15,50 @@ module.exports.userInfo = async (req, res) => {
     if (!ObjectID.isValid(id)) {
         return res.status(400).json({ message: "Id unknown: " + id })
     }
-    
+
     try {
         const user = await UserModel.findById(id).select('-password');
         res.status(200).json(user);
+    } catch (err) {
+        res.status(400).json({ message: "Id unknown: " + id });
+    }
+};
+
+module.exports.updateUser = async (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).json({ message: "Id unknown: " + id })
+    }
+
+    try {
+        const user = await UserModel.findOneAndUpdate(
+            { _id: id, },
+            {
+                $set: {
+                    bio: req.body.bio
+                }
+            },
+            { new: true, upsert: true, setDefaultsOnInsert: true }
+        );
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({ message: "err: " + err });
+    }
+};
+
+module.exports.deleteUser = async (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).json({ message: "Id unknown: " + id })
+    }
+
+    try {
+        const resp = await UserModel.deleteOne({ _id: id });
+        if (resp.deletedCount == 1) {
+            res.status(200).json({ message: 'Successfuly deleted', resp });
+        } else {
+            res.status(400).json({ message: "Id unknown: " + id });
+        }
     } catch (err) {
         res.status(400).json({ message: "Id unknown: " + id });
     }
