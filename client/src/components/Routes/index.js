7571c69bc4dfbd1c3c0aa0df84b7ebaa991e3a55
Idris BEN AHMED <idris.pro83@yahoo.com>
2024-01-20 23:16:54 +0100

    navbar

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from '../../pages/Home';
 import Profil from '../../pages/Profil';
 import Trending from '../../pages/Trending';
+import Navbar from '../Navbar';
 
 function index() {
 
   return(
     <BrowserRouter>
+        <Navbar />
         <Routes>
             <Route path='*' element={<Navigate to='/' />} />
             <Route path="/" element={<Home />} />
